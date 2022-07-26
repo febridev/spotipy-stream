b5c7421ad0b6d210315cb3b0b22513e71f25f3ce
febridev <febriyantabidin@gmail.com>
2022-07-26 18:27:59 +0700

    update test param

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
 
 
 clientid = '751673d086fa4c6d9d764cc4a3cfcf7d'
@@ -13,11 +14,28 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientid,
                                                 scope=scope))
 # sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid,
 #                                                            client_secret=clientsecret))
+tracks=[]
+nowplaying = sp.current_user_playing_track()
+current_playing_type = nowplaying['currently_playing_type']
+if current_playing_type == 'episode':
+    print('podcast')
+    exit()
+else:
+    print('music')
+    demo=nowplaying['item']
+    tracks_name=demo['name']
+    tracks_id=demo['id']
+    tracks_popularity=demo['popularity']
+    tracks_playing=nowplaying['timestamp']
+    print(tracks_name+'-'+str(tracks_popularity))
+# for lsitem in nowplaying['item']:
+#     song_id = lsitem['']
+# print(nowplaying['item',[row]])
+# df = pd.DataFrame(nowplaying[item])
 
-results = sp.current_user_playing_track()
-
+# print(df)
 # for idx, track in enumerate(results['tracks']['items']):
 #     print(idx, track['name'])
 
-print(results)
+# print(results)
 # print(clientsecret)
