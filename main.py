b84ac0948f967d75dd240a238c43fed287f9b75d
febridev <febriyantabidin@gmail.com>
2022-08-06 23:14:39 +0700

    install apache spark

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import pandas as pd
 
 clientid = '751673d086fa4c6d9d764cc4a3cfcf7d'
 clientsecret = '82e0acf2df9244efb2781e261f0e0e94'
-redirect = 'http://localhost'
+redirect = 'http://localhost/callback'
 scope='user-read-recently-played user-read-currently-playing playlist-modify-private'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientid,
-                                                client_secret=clientsecret,
-                                                redirect_uri=redirect,
-                                                scope=scope))
-# sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid,
-#                                                            client_secret=clientsecret))
-tracks=[]
+                                               client_secret=clientsecret,
+                                               redirect_uri=redirect,
+                                               # open_browser=None,
+                                               scope=scope))
+
+# tracks=[]
 nowplaying = sp.current_user_playing_track()
 current_playing_type = nowplaying['currently_playing_type']
 if current_playing_type == 'episode':
@@ -27,7 +27,7 @@ else:
     tracks_id=demo['id']
     tracks_popularity=demo['popularity']
     tracks_playing=nowplaying['timestamp']
-    print(tracks_name+'-'+str(tracks_popularity))
+    print(tracks_name+' - '+str(tracks_popularity)+' - '+str(tracks_playing))
 # for lsitem in nowplaying['item']:
 #     song_id = lsitem['']
 # print(nowplaying['item',[row]])
