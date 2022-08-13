f6d54a6df574192c6c9c39ce80bd48e2b27779df
febridev <febriyantabidin@gmail.com>
2022-08-13 20:32:43 +0700

    final producer

--- main.py
+++ main.py
@@ -2,40 +2,37 @@ import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
+import threading
+from kafka import KafkaProducer
+from json import dumps
+import time
 
+def get_current_play():        
+    clientid = '751673d086fa4c6d9d764cc4a3cfcf7d'
+    clientsecret = '82e0acf2df9244efb2781e261f0e0e94'
+    redirect = 'http://localhost/callback'
+    scope='user-read-recently-played user-read-currently-playing playlist-modify-private'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientid,
+                                                client_secret=clientsecret,
+                                                redirect_uri=redirect,
+                                                # open_browser=None,
+                                                scope=scope))
 
-clientid = '751673d086fa4c6d9d764cc4a3cfcf7d'
-clientsecret = '82e0acf2df9244efb2781e261f0e0e94'
-redirect = 'http://localhost/callback'
-scope='user-read-recently-played user-read-currently-playing playlist-modify-private'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientid,
-                                               client_secret=clientsecret,
-                                               redirect_uri=redirect,
-                                               # open_browser=None,
-                                               scope=scope))
+    tracks=[]
+    nowplaying = sp.current_user_playing_track()
+    np = nowplaying["item"]
+    title = np["name"]
+    popular = np["popularity"]
+    listss = {'title':title, 'popular':popular}
+    tracks.append(listss)
+    producer.send('week6_demo', value=tracks)
+    producer.flush()
+    print(tracks)
 
-# tracks=[]
-nowplaying = sp.current_user_playing_track()
-current_playing_type = nowplaying['currently_playing_type']
-if current_playing_type == 'episode':
-    print('podcast')
-    exit()
-else:
-    print('music')
-    demo=nowplaying['item']
-    tracks_name=demo['name']
-    tracks_id=demo['id']
-    tracks_popularity=demo['popularity']
-    tracks_playing=nowplaying['timestamp']
-    print(tracks_name+' - '+str(tracks_popularity)+' - '+str(tracks_playing))
-# for lsitem in nowplaying['item']:
-#     song_id = lsitem['']
-# print(nowplaying['item',[row]])
-# df = pd.DataFrame(nowplaying[item])
-
-# print(df)
-# for idx, track in enumerate(results['tracks']['items']):
-#     print(idx, track['name'])
-
-# print(results)
-# print(clientsecret)
+# get_current_play()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+while True:
+    threading.Thread(target=get_current_play).start()
+    time.sleep(5)
